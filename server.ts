1eebedae26eea7926163e05586a16fefd0a9d05c
叶聪 <YQCyecong@163.com>
2021-03-05 16:28:57 +0800

    完善server

--- server.ts
+++ server.ts
@@ -5,6 +5,7 @@ import json from 'koa-json'
 import koaBody from 'koa-body'
 import jwt from 'koa-jwt';
 import { JWT_SECRET } from './src/config/index';
+import { formatResult } from './src/middleware/formatResult';
 
 import { createConnection } from 'typeorm';
 
@@ -14,37 +15,22 @@ import { unprotectedRouter, protectedRouter } from './src/routers'
 createConnection().then(() => {
   const app = new Koa()
 
-  app.use(cors())
   app.use(koaBody())
+  app.use(cors())
   app.use(json())
   app.use(logger())
 
-  app.use(async (ctx, next) => {
-    try {
-      await next();
-      if (ctx.status == 404) {
-        ctx.status = 404
-        ctx.body = {
-          code: 404,
-          data: ctx,
-          msg: '资源不存在,请检查 请求路径/请求方式 是否正确!'
-        }
-      }
-    } catch (err) {
-      // 只返回 JSON 格式的响应
-      ctx.status = err.status || 500;
-      ctx.body = { message: err.message };
-    }
-  });
+  app.use(formatResult);
 
   // 无需 JWT Token 即可访问
-  app.use(unprotectedRouter.routes()).use(unprotectedRouter.allowedMethods)
+  app.use(unprotectedRouter.routes()).use(unprotectedRouter.allowedMethods());
 
   // 注册 JWT 中间件
   app.use(jwt({ secret: JWT_SECRET }).unless({ method: 'GET' }));
 
   // 需要 JWT Token 才可访问
-  app.use(protectedRouter.routes()).use(protectedRouter.allowedMethods)
+  app.use(protectedRouter.routes()).use(protectedRouter.allowedMethods());
+
 
   // 监听error
   app.on('error', (err, ctx) => {
